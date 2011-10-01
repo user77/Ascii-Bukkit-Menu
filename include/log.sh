094aba3f2d53820eb1c7df012c2e312852cf2c6b
Jonathan Ross <jonlross@gmail.com>
2011-10-01 11:12:28 -0600

    Layout cleanup

--- include/log.sh
+++ include/log.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Read Config File
 source include/config
+
+# Put somthing into log window so its not empty.
 head -10 $bukkitdir/server.log
+
+# Watch Bukkit Log in screen window Bukkit_Log
 tail -f $bukkitdir/server.log
