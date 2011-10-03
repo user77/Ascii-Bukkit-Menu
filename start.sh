96e4ff343c4d9340c21b07ce359dd64730032a48
Jonathan Ross <jonlross@gmail.com>
2011-10-03 15:15:59 -0600

    added check to adjust screen size if too small

--- start.sh
+++ start.sh
@@ -2,12 +2,19 @@
 
 # Read Config File
 source include/config
+cols=`tput cols`
+lines=`tput lines`
 
 # If Config has not beed edited, then exit.
 if [[ -z $bukkitdir ]]; then
 	echo "Plese Edit include/config"
 	exit 0
 else
+	# If screen size too small, adjust. 
+	if [[ $cols -lt 120 || $lines -lt 40 ]]; then
+		printf '\033[8;40;120t'
+		sleep 0.5 
+	fi
 	# Start the screen sessions.
 	screen -c include/screen.conf
 fi
