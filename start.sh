b376148be824fe25f9e10776f60cc8d25013647b
Jonathan Ross <jonlross@gmail.com>
2011-11-15 17:30:09 -0700

    added banner to start.sh adding installer

--- start.sh
+++ start.sh
@@ -32,10 +32,24 @@ else
 	  done
 	fi
 	# If screen size too small, adjust. 
-	if [[ $cols -lt 120 || $lines -lt 40 ]]; then
-		printf '\033[8;40;120t'
+	if [[ $cols -lt 120 || $lines -lt 42 ]]; then
+		printf '\033[8;42;120t'
 		sleep 0.5 
 	fi
 	# Start the screen sessions.
+
+clear
+echo
+echo "                   _ _   ____        _    _    _ _     __  __                  "
+echo "    /\            (_|_) |  _ \      | |  | |  (_) |   |  \/  |                 "
+echo "   /  \   ___  ___ _ _  | |_) |_   _| | _| | ___| |_  | \  / | ___ _ __  _   _ "
+echo "  / /\ \ / __|/ __| | | |  _ <| | | | |/ / |/ / | __| | |\/| |/ _ \ '_ \| | | |"
+echo " / ____ \\\\__ \ (__| | | | |_) | |_| |   <|   <| | |_  | |  | |  __/ | | | |_| |"
+echo "/_/    \_\___/\___|_|_| |____/ \__,_|_|\_\_|\_\_|\__| |_|  |_|\___|_| |_|\__,_|"
+echo "                                                                               "
+echo
+sleep 1
+clear
+
 	screen -c include/screen.conf
 fi
